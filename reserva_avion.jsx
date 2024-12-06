15036784b15be5f174521f85352b994a06bb6464
jul213 <158296107+jul213@users.noreply.github.com>
2024-12-06 11:42:21 +0000

    creacion de reserva_avion

---
+++ reserva_avion.jsx
@@ -0,0 +1,12 @@
+import "./reserva_avion.css"
+import { useState } from "react"
+import { useEffect } from "react"
+
+function aeropuerto(){
+    const [abierto, cerrado] = useState(null)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        
+    })
+}
