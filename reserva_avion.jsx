2f6b98248d21ce804091c2b831477e18c6765002
jul213 <158296107+jul213@users.noreply.github.com>
2024-12-06 11:58:51 +0000

    verificar horario

--- reserva_avion.jsx
+++ reserva_avion.jsx
@@ -3,10 +3,20 @@ import { useState } from "react"
 import { useEffect } from "react"
 
 function aeropuerto(){
-    const [abierto, cerrado] = useState(null)
+    const [abierto, setAbierto] = useState(false)
     const [error, setError] = useState(null)
 
     useEffect(() => {
-        
+        console.log("bienvenidos al aeropuerto barajas")
+        const verificarHorario = () => {
+            const ahora = new Date();
+            const hora = ahora.getHours();
+            if (hora > 6 && hora < 24){
+                setAbierto(true)
+            } else{
+                setAbierto(false)
+            }
+        }
+        verificarHorario()
     })
 }
