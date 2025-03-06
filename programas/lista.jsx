4a9205d7b714d35fdb77710c35c8aaa454f43882
jul213 <158296107+jul213@users.noreply.github.com>
2025-03-06 22:41:16 +0000

    creacion de funcion Garage

--- programas/lista.jsx
+++ programas/lista.jsx
@@ -1,3 +1,15 @@
 function Car(props){
     return <li>i am {props.brand} </li>
 }
+
+function Garage(){
+    const cars = ['ford','bmw','audi','MClaren'];
+    return (
+        <>
+            <h1>who cars in my garage?</h1>
+            <ul>
+                
+            </ul>
+        </>
+    )
+}
