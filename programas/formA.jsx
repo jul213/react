cfb6994df3cd8a5cd38622801cf432c0f0cb9213
julio <158296107+jul213@users.noreply.github.com>
2025-03-09 22:02:07 +0000

    creacion de myformt

--- programas/formA.jsx
+++ programas/formA.jsx
@@ -24,3 +24,26 @@ function MyForm(){
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<MyForm />);
+
+
+
+function MyFormT(){
+    const [textarea, setTextArea] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`el nombre que ingresaste es: ${name}`)
+        }
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <label>Ingresa tu nombre: 
+                    <input type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} />
+                </label>
+                <input type="submit" />
+            </form>
+        )
+}
+
