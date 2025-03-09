a59f671da530c0ac0349b5a81269b4c32f31b242
julio <158296107+jul213@users.noreply.github.com>
2025-03-09 21:54:29 +0000

    creacion de handlesubmit

--- programas/formA.jsx
+++ programas/formA.jsx
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom/client";
 
 function MyForm(){
     const [name, setName] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`el nombre que ingresaste es: `)
+        }
 }
