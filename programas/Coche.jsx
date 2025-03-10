48610086a4f3e2a507d4d9785f98b3f8ff32c51d
julio <158296107+jul213@users.noreply.github.com>
2025-03-10 07:58:12 +0000

    renderizamso componente

--- programas/Coche.jsx
+++ programas/Coche.jsx
@@ -1,3 +1,12 @@
 class Car extends React.Component{
-    
+
+     Sofia(props){
+       return  <h1>sofia es {props} </h1>
+    }
+
+    render(){
+        <>
+            <Sofia name="Osita comelona"/>
+        </>
+    }
 }
