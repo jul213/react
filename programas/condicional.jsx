67f200105e4a79207744b8283eb807b7d4bbc554
julio <158296107+jul213@users.noreply.github.com>
2025-03-12 21:01:03 +0000

    termionamos el return

--- programas/condicional.jsx
+++ programas/condicional.jsx
@@ -3,7 +3,11 @@ function pepe(props){
     return (
         <>
             <h1>GARAGE</h1>
-            
+            {cars.length > 0 &&
+            <h2>
+                tienes los siguientes coches {cars.length} en tu garage
+            </h2>
+            }
         </>
     )
 }
