db43980b2a7909cfe1f0523344f376d2404a91d3
julio <158296107+jul213@users.noreply.github.com>
2025-03-13 07:19:23 +0000

    creacion de handleSubmit para el envio del formulario

--- programas/componen.jsx
+++ programas/componen.jsx
@@ -11,4 +11,9 @@ function form(){
         const value = event.target.value;
         setInputs(values => ({...values, [name]: value}))
     }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(inputs);
+    }
 }
