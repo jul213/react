91f22d4b9f2c1a18c34206786ee1f2a085a4a7f3
julio <158296107+jul213@users.noreply.github.com>
2025-03-13 07:17:00 +0000

    hacemos el handleChange

--- programas/componen.jsx
+++ programas/componen.jsx
@@ -4,4 +4,11 @@ import ReactDOM from "react-dom/client";
 function form(){
 
     const [inputs, setInputs] = useState({});
+
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
 }
