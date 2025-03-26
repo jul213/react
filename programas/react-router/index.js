e3d042c047b3dfe27b089ae68ea85bf57ccc7797
julio <158296107+jul213@users.noreply.github.com>
2025-03-26 23:08:06 +0000

    fix errores

--- programas/react-router/index.js
+++ programas/react-router/index.js
@@ -12,7 +12,6 @@ export default function App(){
             <Routes>
                 <Route path="/" element={<Layout/>}>
                 <Route index element={<Home/>} />
-                <Route path="/" element={<Layout/>} />
                 <Route path="blogs" element={<Blogs/>} />
                 <Route path="contact" element={<Contact/>} />
                 <Route path="*" element={<NoPage/>} />
