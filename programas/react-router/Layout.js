ff8816847aa121d3d7702730e24c6b226e962ff2
julio <158296107+jul213@users.noreply.github.com>
2025-03-26 22:48:32 +0000

    creacion de nav e ul

--- programas/react-router/Layout.js
+++ programas/react-router/Layout.js
@@ -1,2 +1,13 @@
 import {Outlet, Link} from "react-roter-dom";
 
+const Layout = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    
+                </ul>
+            </nav>
+        </>
+    )
+}
