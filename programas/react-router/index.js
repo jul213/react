cb4dc58097f3b4fb1df025e9f84b24a049b3dcc7
julio <158296107+jul213@users.noreply.github.com>
2025-03-26 23:00:51 +0000

    importaciones

---
+++ programas/react-router/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import {BrowseRouter, Routes, Route} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Home";
+import Blogs from "./Blogs";
+import Contact from "./Contact";
+import NoPage from "./NoPage";
