3ac3302aced6b9ca6f87f19fe463e617f9abcf32
julio <158296107+jul213@users.noreply.github.com>
2025-04-05 16:05:22 +0000

    creaccion del reducer

--- programas/reducer.jsx
+++ programas/reducer.jsx
@@ -20,6 +20,14 @@ const initialTodos = [
 const reducer = (state, action) => {
     switch(action.type){
         case "COMPLETE":
-            
+            return state.map((todo) => {
+                if (todo.id === action.id){
+                    return {...todo, complete: !todo.complete}
+                } else {
+                    return todo;
+                }
+            })
+        default:
+            return state
     }
 }
