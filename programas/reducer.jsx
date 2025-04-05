e80ad84d2cfc11cbdf3efcba6fa482492b2d568f
julio <158296107+jul213@users.noreply.github.com>
2025-04-05 15:21:25 +0000

    creacccion de initialTodos

--- programas/reducer.jsx
+++ programas/reducer.jsx
@@ -1,2 +1,19 @@
 import { useReducer } from "react";
 import ReactDOM from "react-dom/client";
+
+
+const initialTodos = [
+    {
+        id: 1,
+        title: "Todo 1",
+        complete: false,
+    },
+
+    {
+        id: 2,
+        title: "Todo 2",
+        complete: false
+    },
+];
+
+
